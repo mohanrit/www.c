84dbfd412946386d0b8aeb7e123047a0cfd0091a
mohanrit <35220535+mohanrit@users.noreply.github.com>
2018-01-31 15:53:30 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a[20],i,n;
+	printf("enter the no.");
+  
+	scanf("%d",&n);
+	printf("enter the array");
+	for(i=0;i<n;i++)
+	{
+  	printf("%d %d\n",a[i],i); 
+}
+}
